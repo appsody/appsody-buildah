0f33c76c06a2a950a4da1345e77a6bc5dc820d7c
Michele Chilanti <chilantim@gmail.com>
2019-09-04 17:07:42 -0500

    Eliminated sed for --build-arg

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN yum -y update && \
       yum -y install wget
 # Matching appsody binary does not exist in upstream.
 # Provide the proper version once it is available.
+ARG CLI_VERSION
 RUN wget https://github.com/appsody/appsody/releases/download/CLI_VERSION/appsody-CLI_VERSION-1.x86_64.rpm
 RUN yum localinstall -y ./appsody-CLI_VERSION-1.x86_64.rpm
 RUN chmod +x extract.sh
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #CLI_VERSION
 #TRAVIS_TAG
 set -e
-sed -i  "s/CLI_VERSION/$1/g" ./Dockerfile
+#sed -i  "s/CLI_VERSION/$1/g" ./Dockerfile
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_ORG/appsody-buildah:$2 .
+docker build -t $DOCKER_ORG/appsody-buildah:$2 --build-arg CLI_VERSION=$1 . 
 docker push $DOCKER_ORG/appsody-buildah:$2
